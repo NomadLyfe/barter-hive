69ee28343acd4b80f20970706b0111f059705ab2
Jeremy Garcia <jeremygarcia9400@gmail.com>
2024-02-13 19:57:33 -0500

    initial commit

---
+++ server/migrations/versions/34ce00c62ed3_initial_db.py
@@ -0,0 +1,24 @@
+"""initial db
+
+Revision ID: 34ce00c62ed3
+Revises: 
+Create Date: 2024-02-13 19:57:21.828808
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '34ce00c62ed3'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
