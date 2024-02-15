4a6e0f1b093f3591a6aae76bdde8ad1876456e8d
Jeremy Garcia <jeremygarcia9400@gmail.com>
2024-02-14 22:49:14 -0500

    added imports and index route in app.py

--- server/app.py
+++ server/app.py
@@ -1,4 +1,14 @@
-from config import app
+from config import app, db, Api
+from flask import request, session, render_template
+from flask_restful import Resource
+import requests
+
+@app.route('/')
+@app.route('/<int:id>')
+def index(id=0):
+    return render_template("index.html")
+
+# routes go here
 
 if __name__ == "__main__":
-  app.run(port=5555, debug=True)
+    app.run(port=5555, debug=True)
