fc94e11cf79d2cd811ca0895b082c0f784b8e777
Jeremy Garcia <jeremygarcia9400@gmail.com>
2024-02-14 22:55:07 -0500

    added imports to seed.py

--- server/seed.py
+++ server/seed.py
@@ -1,6 +1,8 @@
 from config import app, db
+from faker import Faker
+from random import randint, choice
 
 if __name__ == "__main__":
-  with app.app_context():
-    pass
-    # remove pass and write your seed data
+    with app.app_context():
+        pass
+        # remove pass and write your seed data
