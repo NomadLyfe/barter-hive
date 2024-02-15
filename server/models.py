68cbec7b7830e4493902dfc2f069278742feaef3
Jeremy Garcia <jeremygarcia9400@gmail.com>
2024-02-14 23:15:04 -0500

    created User class model in models.py

--- server/models.py
+++ server/models.py
@@ -5,4 +5,22 @@ from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.orm import validates
 import re
 
-# model classes here
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String)
+    email = db.Column(db.String, unique=True, nullable=False)
+    profile_pic = db.Column(db.String)
+    banner_pic = db.Column(db.String)
+    bday = db.Column(db.String, nullable=False)
+    phone = db.Column(db.String, nullable=False)
+    reputation = db.Column(db.Integer)
+    city = db.Column(db.String, nullable=False)
+    state = db.Column(db.String, nullable=False)
+    country = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return f'User {self.username}, ID {self.id}'
+    
